d3f6bf76be7a81b897131b8fac4d04c2ecf82a43
kouki515 <71614240+kouki515@users.noreply.github.com>
2025-09-04 16:41:35 +0900

    metadataを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "峰田ソーイング | ホーム",
   description: "高品質な縫製であなたのブランドをサポート。峰田ソーイングは小ロット対応から量産まで、国内一貫生産体制でお客様のニーズにお応えします。",
+  verification: {
+    google: "XX-preis2eZSbSOHl9AuxkWJ-1f_mEGhtTrqGsTf_xA",
+  },
 };
 
 export default function RootLayout({
